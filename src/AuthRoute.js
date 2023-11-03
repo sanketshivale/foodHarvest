88c54a63a669eab561fcb9aeb6153b27819a3bc1
= <=>
2023-11-04 02:25:40 +0530

    Add useEffect and useState hooks to AuthRoute component for Firebase authentication

--- src/AuthRoute.js
+++ src/AuthRoute.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import firebase from './firebase-config';
 
 const AuthRoute = () => {
-    
-  const currentUser = firebase.auth().currentUser;
-  console.log(currentUser);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unsubscribe(); // Unsubscribe the listener when the component unmounts
+    };
+  }, []);
+
+  if (currentUser === null) {
+    // Firebase is still checking the authentication state, you can show a loading spinner here
+    return <center>Loading...</center>;
+  }
+
   return currentUser ? <Outlet /> : <Navigate to="/login" />;
 };
 
